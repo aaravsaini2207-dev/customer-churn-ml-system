06895bd5a32ea34fe9e70cd9cb85d52942662f13
User <user@example.com>
2026-09-22 16:34:27 +0530

    Make Firebase optional for CI tests

--- App/firebase.py
+++ App/firebase.py
@@ -7,13 +7,14 @@ from firebase_admin import credentials
 
 firebase_json = os.getenv("FIREBASE_SERVICE_ACCOUNT")
 
-if not firebase_json:
-    raise RuntimeError(
-        "FIREBASE_SERVICE_ACCOUNT environment variable is not set."
-    )
+if firebase_json:
+    firebase_config = json.loads(firebase_json)
+    cred = credentials.Certificate(firebase_config)
 
-firebase_config = json.loads(firebase_json)
+    if not firebase_admin._apps:
+        app = firebase_admin.initialize_app(cred)
+    else:
+        app = firebase_admin.get_app()
+else:
+    app = None
 
-cred = credentials.Certificate(firebase_config)
-
-app = firebase_admin.initialize_app(cred)
